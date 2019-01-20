8d288c10def5bccf8651d1358e0c57031a58f9c1
Joona Heikkilä <joona.o.heikkila@gmail.com>
2019-01-21 00:44:13 +0200

    Bring back loading message

--- commands.js
+++ commands.js
@@ -1,11 +1,37 @@
 const { allowedToSendToChatAlready } = require('./flood-control')
 const api = require('./api')
 
+/**
+ * Sends a "Loading..." message, then removes it after the action has finished.
+ * @param {TelegramClient} tgClient
+ * @param {string} chatId
+ * @param {() => Promise<void>} action
+ * @returns {Promise<void>}
+ */
+const doWithLoadingMessage = async (tgClient, chatId, action) => {
+  const opts = {
+    parse_mode: 'markdown',
+    disable_notification: true
+  }
+
+  const loadingMessage = await tgClient.sendMessage(
+    chatId,
+    '_Kisse lataa..._',
+    opts
+  )
+  await action()
+  await tgClient.deleteMessage(chatId, loadingMessage.message_id)
+}
+
 const createCatCommand = (tgClient, getApiUrl) => msg => {
   const chatId = msg.chat.id
   if (!allowedToSendToChatAlready(chatId)) return
 
-  tgClient.sendPhoto(chatId, getApiUrl())
+  doWithLoadingMessage(
+    tgClient,
+    chatId,
+    async () => await tgClient.sendPhoto(chatId, getApiUrl())
+  )
 }
 
 const attachCommands = tgClient => {
