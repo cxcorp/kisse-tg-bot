023ad19313252ed7e552d1368a224a617748050c
Joona Heikkilä <joona.o.heikkila@gmail.com>
2019-01-21 00:53:12 +0200

    Fix cat gif showing as static image
    Apparently the sendPhoto method is only for static videos. Fixes cat gifs by
    using sendVideo for gifs.

--- commands.js
+++ commands.js
@@ -23,23 +23,40 @@ const doWithLoadingMessage = async (tgClient, chatId, action) => {
   await tgClient.deleteMessage(chatId, loadingMessage.message_id)
 }
 
-const createCatCommand = (tgClient, getApiUrl) => msg => {
+const createCatPhotoCommand = tgClient => msg => {
   const chatId = msg.chat.id
   if (!allowedToSendToChatAlready(chatId)) return
 
-  doWithLoadingMessage(
+  return doWithLoadingMessage(
     tgClient,
     chatId,
-    async () => await tgClient.sendPhoto(chatId, getApiUrl())
+    async () => await tgClient.sendPhoto(chatId, api.getCatPictureUrl())
+  )
+}
+
+const createCatGifCommand = tgClient => msg => {
+  const chatId = msg.chat.id
+  if (!allowedToSendToChatAlready(chatId)) return
+
+  return doWithLoadingMessage(
+    tgClient,
+    chatId,
+    async () =>
+      await tgClient.sendVideo(
+        chatId,
+        api.getCatGifUrl(),
+        {},
+        {
+          filename: `kisse_gif_${new Date().getTime()}.gif`,
+          contentType: 'image/gif'
+        }
+      )
   )
 }
 
 const attachCommands = tgClient => {
-  tgClient.onText(
-    /^\/kisse\b/,
-    createCatCommand(tgClient, api.getCatPictureUrl)
-  )
-  tgClient.onText(/^\/kissegif\b/, createCatCommand(tgClient, api.getCatGifUrl))
+  tgClient.onText(/^\/kisse\b/, createCatPhotoCommand(tgClient))
+  tgClient.onText(/^\/kissegif\b/, createCatGifCommand(tgClient))
 }
 
 module.exports = {
